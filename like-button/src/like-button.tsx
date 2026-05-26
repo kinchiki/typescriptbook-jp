0dc006cc4af6c6324535e2fab1767b2dfc2a6808
Akito Naito <17106187+kinchiki@users.noreply.github.com>
2026-05-26 12:03:52 +0900

    いいねボタンのカウントアップを実装

--- like-button/src/like-button.tsx
+++ like-button/src/like-button.tsx
@@ -1,9 +1,13 @@
 import "./like-button.css";
+import { useState } from "react";
 
 export function LikeButton() {
-  const count = 999;
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
-    <button className="like" type="button">
+    <button className="like" onClick={handleClick} type="button">
       {count}
     </button>
   );
